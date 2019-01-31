0a3e0db533d5181e5447fbaf672d454d3a2852f3
deboraornellas <debora.ornellas1@gmail.com>
2019-01-30 23:20:20 -0800

    First hard-coded version of components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-class Word extends React.Component {
+let wordList = [
+    {"word": "amour", "translation": "love", "category": "nouns"},
+    {"word": "chat", "translation": "cat", "category": "animals"}
+]
 
-}
+const Word = ({word="No Word", translation= "Not available", category = "None"}) => {
+	return (
+		<section>
+			<h2>{word}</h2>
+			<p>Translation: {translation}</p>
+			<p>Category: {category}</p>
+		</section>
+	)
+    }
 
 class ListWords extends React.Component {
     render() {
+        const { words } = this.props
         return (
-            <h1> Oieee </h1>
+            <div>
+                <h1> Hi there! This is my french vocab list: </h1>
+                {words.map(
+                    (word, i) => 
+                        <Word
+                            key={i}
+                            word={word.word} 
+                            translation={word.translation} 
+                            category={word.category}/>
+                )}
+            </div>
         )
     }
 }
 
 render(
-    <ListWords />,
+    <ListWords words={wordList}/>,
     document.getElementById('root')
 )
